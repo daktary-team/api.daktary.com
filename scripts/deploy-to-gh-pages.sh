4509fff445238611b84a260347c0c78a2e4cfe2d
Travis CI <stephane.langlois@scopyleft.fr>
2016-11-21 19:56:41 +0100

    test

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -2,29 +2,6 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
-
 rev=$(git rev-parse --short HEAD)
 
-mkdir -p deploy/gh-pages
-cd deploy/gh-pages
-
-git init
-git config user.name "Stéphane Langlois"
-git config user.email "stephane@scopyleft.fr"
-
-git remote add upstream "https://$GITHUB_TOKEN@github.com/daktary-team/api.daktary.com.git"
-git fetch upstream
-git reset upstream/gh-pages
-
-echo "api.daktary.com" > CNAME
-ls -l
-touch .
-
-git add -A .
-git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+ls
