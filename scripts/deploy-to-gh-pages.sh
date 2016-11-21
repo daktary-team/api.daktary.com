77bbcd40dc5b3312e9e4c3ddcb59a1ccc176699c
Travis CI <stephane.langlois@scopyleft.fr>
2016-11-21 20:05:10 +0100

    test

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -4,4 +4,24 @@ set -o errexit -o nounset
 
 rev=$(git rev-parse --short HEAD)
 
-ls
+mkdir -p deploy/gh-pages
+mkdir -p deploy/master
+cd deploy/master
+
+npm test
+
+git init
+git config user.name "Stéphane Langlois"
+git config user.email "stephane@scopyleft.fr"
+
+git remote add upstream "https://$GITHUB_TOKEN@github.com/daktary-team/api.daktary.com.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+echo "api.daktary.com" > CNAME
+ls -l
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
