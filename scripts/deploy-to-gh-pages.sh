885a905447cc6e0f30ef9814c772ace22ab54d8d
Travis CI <stephane.langlois@scopyleft.fr>
2016-11-21 19:24:06 +0100

    Tets

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -10,22 +10,11 @@ fi
 
 rev=$(git rev-parse --short HEAD)
 
-rm -Rf stage/_book
-mkdir -p stage/_book
-cd stage/_book
+mkdir -p gh-pages
+cd gh-pages
 
 git init
 git config user.name "Stéphane Langlois"
 git config user.email "stephane@scopyleft.fr"
 
-git remote add upstream "https://$GH_TOKEN@github.com:$GITHUB_REPO"
-git fetch upstream
-git reset upstream/gh-pages
 
-echo "api.daktary.com" > CNAME
-
-touch .
-
-git add -A .
-git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
