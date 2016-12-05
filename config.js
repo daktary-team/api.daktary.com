9de75a920011f7b8521cb0acbe247b3a37c22419
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-05 22:52:44 +0100

    Add github basic authentification with heroku config variables

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {}
+
+// Please read Github instructions to complete
+// https://developer.github.com/v3/#increasing-the-unauthenticated-rate-limit-for-oauth-applications
+config.ghSecret = process.env.GITHUB_SECRETS
+config.ghId = process.env.GITHUB_ID
+
+module.exports = config
