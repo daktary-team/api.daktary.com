128339b424051b61c8e372254bb97f4f747cb10c
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-06 13:17:28 +0100

    test

--- routes.js
+++ routes.js
@@ -1,17 +1,50 @@
-'use strict'
-
 const express = require('express')
 const app = express()
 const https = require('https')
-const port = process.env.PORT || 8001
+const config = require('./config')
+
+
+/*
+ * Configure the parameters for the api Github Url
+ */
+const apiUrl = {}
+apiUrl.root = 'https://api.github.com'
+/**
+ * Create query for api url
+ * @param {Object} config   config data.
+ * @return {String} query   query string with ref=branch&client_id&client_secret.
+ */
+apiUrl.query = (cfg) =>
+    '?ref=master' + apiUrl.addAuth(this.query, cfg)
+/**
+ * Create query complement to Github authorization
+ * @param {Object} config   config data {ghId, ghSecret}.
+ * @return {String} query   query string with &client_id&client_secret.
+ */
+apiUrl.addAuth = (query, {ghId, ghSecret}) =>
+    (typeof ghId !== 'undefined' && typeof ghSecret !== 'undefined')
+    ? `&client_id=${ghId}&client_secret=${ghSecret}`
+    : ''
+
 
 app.get('/', (req, res) => {
-    const routes = {
-        repos_url: 'https://api.github.com/{owner}'
-    }
     res.header('Access-Control-Allow-Origin', '*')
-
+    const routes = {
+        repos_url: `${apiUrl.root}/{owner}`
+    }
     res.send(routes)
 })
 
-app.listen(port)
+app.get('/:owner/:repo', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    const routes = {
+        git_url:
+            `${apiUrl.root}/repos/` +
+            `${req.params.owner}/` +
+            `${req.params.repo}` +
+            apiUrl.query(config)
+    }
+    res.send(routes)
+})
+
+app.listen(process.env.PORT)
