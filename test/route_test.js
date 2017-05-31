4beeccb8965b4d87d82d1b7de31fb62f089f103e
Travis CI <stephane.langlois@scopyleft.fr>
2017-05-31 16:08:19 +0200

    Test chai

--- test/route_test.js
+++ test/route_test.js
@@ -1,11 +1,13 @@
-'use strict'
+const chai = require('chai')
+const routes = require('../routes')
 
-const assert = require('assert')
+const expect = chai.expect
 
 describe('api', () =>
     describe('GET /', () =>
         it('respond with an array of users', () => {
-            // ...
+
+            expect("ee").to.be.a('string')
         })
     )
 )
