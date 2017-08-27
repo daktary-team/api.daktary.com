35c3e6ce5ae2092288d95091ff9ed8b33b327203
Travis CI <stephane.langlois@scopyleft.fr>
2017-08-27 09:35:49 +0200

    Fix typo in config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {}
 
 // Please read Github instructions to complete
 // https://developer.github.com/v3/#increasing-the-unauthenticated-rate-limit-for-oauth-applications
-config.ghSecret = process.env.GITHUB_SECRETS
+config.ghSecret = process.env.GITHUB_SECRET
 config.ghId = process.env.GITHUB_ID
 
 module.exports = config
